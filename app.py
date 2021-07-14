020df60cbe51c5d3771c44153cf1b40cdfe38319
nuwanuom18 <nuwan.18@cse.mrt.ac.lk>
2021-07-14 23:18:06 +0530

    release

---
+++ app.py
@@ -0,0 +1,13 @@
+from flsk import Flask, jsonify, request
+import time
+
+app = Flask(__name__)
+@app.roure("/bot", method=["POST"])
+
+def response():
+    query = dict(request.form)['query']
+    result = query + " "+ time.ctime()
+    return jsonify({"response": result})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
