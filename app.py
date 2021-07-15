342f986479499bbaade5d52603dcc07dfc10a357
nuwanuom18 <nuwan.18@cse.mrt.ac.lk>
2021-07-15 13:36:44 +0530

    change message

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def response():
     query = dict(request.form)['query']
     result = query + " "+ time.ctime()
-    return jsonify({"response": result})
+    return "HelloNuwan" #jsonify({"response": result})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",)
