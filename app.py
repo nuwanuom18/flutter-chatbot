071e38ab27e1f81587f346913d028e1c3e1e82f2
nuwanuom18 <nuwan.18@cse.mrt.ac.lk>
2021-07-15 14:32:28 +0530

    error fixed

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flsk import Flask, jsonify, request
 import time
 
 app = Flask(__name__)
-@app.roure("/bot", method=["POST"])
+@app.route("/bot", method=["POST"])
 
 def response():
     query = dict(request.form)['query']
     result = query + " "+ time.ctime()
-    return "HelloNuwan" #jsonify({"response": result})
+    return jsonify({"response": "Hellobn"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",)
